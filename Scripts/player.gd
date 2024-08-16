50240b066c4975eda2701639cae34ea5ae1d2737
Griffin Baxter <GriffinRBaxter@gmail.com>
2024-08-17 08:47:05 +1200

    Basic Godot project setup
    Basic movement, some text, object collision. Using a viewport stretch mode to make scaling the game view (ironic due to the game jam theme).

---
+++ Scripts/player.gd
@@ -0,0 +1,17 @@
+extends CharacterBody2D
+
+const SPEED = 300.0
+
+
+func _physics_process(_delta: float) -> void:
+	var direction := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.y = direction.y * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+
+
+func _process(_delta: float) -> void:
+	move_and_slide()
