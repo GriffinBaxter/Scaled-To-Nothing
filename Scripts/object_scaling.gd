baba323fd768e5552af0173e9dfa89b9fda36f5d
Griffin Baxter <GriffinRBaxter@gmail.com>
2024-08-17 14:41:45 +1200

    Cannot scale up object if colliding with wall/player

--- Scripts/object_scaling.gd
+++ Scripts/object_scaling.gd
@@ -1,9 +1,9 @@
 extends Area2D
 
-
 const SCALE_SPEED = 0.1
 
 var mouse_entered_object = false
+var body_entered_object = false
 var original_scale = Vector2(1, 1)
 
 
@@ -12,21 +12,37 @@ func _ready() -> void:
 
 
 func _input(event: InputEvent) -> void:
-	if event is InputEventMouseMotion and mouse_entered_object and Input.is_action_pressed("left_mouse"):
+	if (
+		event is InputEventMouseMotion
+		and mouse_entered_object
+		and Input.is_action_pressed("left_mouse")
+	):
 		var scale_amount = SCALE_SPEED
 		var global_mouse_position = get_global_mouse_position()
 
 		# top left
-		if (global_mouse_position - global_position).x < 0 and (global_mouse_position - global_position).y < 0:
+		if (
+			(global_mouse_position - global_position).x < 0
+			and (global_mouse_position - global_position).y < 0
+		):
 			scale_amount *= -event.relative.x - event.relative.y
 		# top right
-		elif (global_mouse_position - global_position).x > 0 and (global_mouse_position - global_position).y < 0:
+		elif (
+			(global_mouse_position - global_position).x > 0
+			and (global_mouse_position - global_position).y < 0
+		):
 			scale_amount *= event.relative.x - event.relative.y
 		# bottom left
-		elif (global_mouse_position - global_position).x < 0 and (global_mouse_position - global_position).y > 0:
+		elif (
+			(global_mouse_position - global_position).x < 0
+			and (global_mouse_position - global_position).y > 0
+		):
 			scale_amount *= -event.relative.x + event.relative.y
 		# bottom right
-		elif (global_mouse_position - global_position).x > 0 and (global_mouse_position - global_position).y > 0:
+		elif (
+			(global_mouse_position - global_position).x > 0
+			and (global_mouse_position - global_position).y > 0
+		):
 			scale_amount *= event.relative.x + event.relative.y
 		else:
 			scale_amount *= 0
@@ -36,7 +52,7 @@ func _input(event: InputEvent) -> void:
 			scale = original_scale * 2
 		elif scale + to_scale < original_scale * 0.5:
 			scale = original_scale * 0.5
-		else:
+		elif scale_amount < 0 or (scale_amount >= 0 and not body_entered_object):
 			scale += to_scale
 
 
@@ -46,3 +62,11 @@ func _on_mouse_entered() -> void:
 
 func _on_mouse_exited() -> void:
 	mouse_entered_object = false
+
+
+func _on_body_entered(_body: Node2D) -> void:
+	body_entered_object = true
+
+
+func _on_body_exited(_body: Node2D) -> void:
+	body_entered_object = false
