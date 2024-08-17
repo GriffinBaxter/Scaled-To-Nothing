f4a8d77501b9b57e8d5a61d1a5479a8ac0494fe0
Griffin Baxter <GriffinRBaxter@gmail.com>
2024-08-17 16:53:32 +1200

    Added corners when scaling

--- Scripts/object_scaling.gd
+++ Scripts/object_scaling.gd
@@ -1,23 +1,59 @@
 extends Area2D
 
+enum CornerPos { NONE, TOP_LEFT, TOP_RIGHT, BOTTOM_LEFT, BOTTOM_RIGHT }
+
 const SCALE_SPEED = 0.1
 const POSITION_OFFSET = 3.25
 
 var mouse_entered_object = false
 var body_entered_object = false
 var original_scale = Vector2(1, 1)
+var selected_corner: CornerPos = CornerPos.NONE
 
 
 func _ready() -> void:
 	original_scale = scale
 
 
+func _draw() -> void:
+	const OFFSET = 3.
+	const SQUARE_SIZE = Vector2(1.5, 1.5)
+	const ACTIVE_COLOR = Color(1, 1, 1, 1)
+	var color = Color(1, 1, 1, 0.5) if mouse_entered_object else Color(1, 1, 1, 0.1)
+	draw_set_transform(Vector2(-position.x, -position.y))
+
+	# top left
+	var top_left_rect = Rect2(
+		Vector2(
+			global_position.x - OFFSET - SQUARE_SIZE.x, global_position.y - OFFSET - SQUARE_SIZE.y
+		),
+		SQUARE_SIZE
+	)
+	draw_rect(top_left_rect, ACTIVE_COLOR if selected_corner == CornerPos.TOP_LEFT else color)
+
+	# top right
+	var top_right_rect = Rect2(
+		Vector2(global_position.x + OFFSET, global_position.y - OFFSET - SQUARE_SIZE.y), SQUARE_SIZE
+	)
+	draw_rect(top_right_rect, ACTIVE_COLOR if selected_corner == CornerPos.TOP_RIGHT else color)
+
+	# bottom left
+	var bottom_left_rect = Rect2(
+		Vector2(global_position.x - OFFSET - SQUARE_SIZE.x, global_position.y + OFFSET), SQUARE_SIZE
+	)
+	draw_rect(bottom_left_rect, ACTIVE_COLOR if selected_corner == CornerPos.BOTTOM_LEFT else color)
+
+	# bottom right
+	var bottom_right_rect = Rect2(
+		Vector2(global_position.x + OFFSET, global_position.y + OFFSET), SQUARE_SIZE
+	)
+	draw_rect(
+		bottom_right_rect, ACTIVE_COLOR if selected_corner == CornerPos.BOTTOM_RIGHT else color
+	)
+
+
 func _input(event: InputEvent) -> void:
-	if (
-		event is InputEventMouseMotion
-		and mouse_entered_object
-		and Input.is_action_pressed("left_mouse")
-	):
+	if event is InputEventMouseMotion and mouse_entered_object:
 		var scale_amount = SCALE_SPEED
 		var to_move_position = Vector2(0, 0)
 		var global_mouse_position = get_global_mouse_position()
@@ -27,6 +63,7 @@ func _input(event: InputEvent) -> void:
 			(global_mouse_position - global_position).x < 0
 			and (global_mouse_position - global_position).y < 0
 		):
+			update_corner(CornerPos.TOP_LEFT)
 			scale_amount *= -event.relative.x - event.relative.y
 			to_move_position = Vector2(-POSITION_OFFSET, -POSITION_OFFSET)
 
@@ -35,6 +72,7 @@ func _input(event: InputEvent) -> void:
 			(global_mouse_position - global_position).x > 0
 			and (global_mouse_position - global_position).y < 0
 		):
+			update_corner(CornerPos.TOP_RIGHT)
 			scale_amount *= event.relative.x - event.relative.y
 			to_move_position = Vector2(POSITION_OFFSET, -POSITION_OFFSET)
 
@@ -43,6 +81,7 @@ func _input(event: InputEvent) -> void:
 			(global_mouse_position - global_position).x < 0
 			and (global_mouse_position - global_position).y > 0
 		):
+			update_corner(CornerPos.BOTTOM_LEFT)
 			scale_amount *= -event.relative.x + event.relative.y
 			to_move_position = Vector2(-POSITION_OFFSET, POSITION_OFFSET)
 
@@ -51,28 +90,39 @@ func _input(event: InputEvent) -> void:
 			(global_mouse_position - global_position).x > 0
 			and (global_mouse_position - global_position).y > 0
 		):
+			update_corner(CornerPos.BOTTOM_RIGHT)
 			scale_amount *= event.relative.x + event.relative.y
 			to_move_position = Vector2(POSITION_OFFSET, POSITION_OFFSET)
 
 		else:
+			selected_corner = CornerPos.NONE
 			scale_amount *= 0
 
-		var to_scale = Vector2(scale_amount, scale_amount)
-		if scale + to_scale > original_scale * 2:
-			scale = original_scale * 2
-		elif scale + to_scale < original_scale * 0.5:
-			scale = original_scale * 0.5
-		elif scale_amount < 0 or (scale_amount >= 0 and not body_entered_object):
-			scale += to_scale
-			position += to_move_position * scale_amount
+		if Input.is_action_pressed("left_mouse"):
+			var to_scale = Vector2(scale_amount, scale_amount)
+			if scale + to_scale > original_scale * 2:
+				scale = original_scale * 2
+			elif scale + to_scale < original_scale * 0.5:
+				scale = original_scale * 0.5
+			elif scale_amount < 0 or (scale_amount >= 0 and not body_entered_object):
+				scale += to_scale
+				position += to_move_position * scale_amount
+
+
+func update_corner(corner: CornerPos) -> void:
+	if selected_corner != corner:
+		selected_corner = corner
+		queue_redraw()
 
 
 func _on_mouse_entered() -> void:
 	mouse_entered_object = true
+	queue_redraw()
 
 
 func _on_mouse_exited() -> void:
 	mouse_entered_object = false
+	update_corner(CornerPos.NONE)
 
 
 func _on_body_entered(_body: Node2D) -> void:
