29de3bda52dfd5339b1432249433936c5e29c49e
Griffin Baxter <GriffinRBaxter@gmail.com>
2024-08-18 21:40:41 +1200

    Add main menu sound

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -1,5 +1,11 @@
 extends Control
 
+@onready var main_menu_sound: AudioStreamPlayer = $MainMenuSound
+
+
+func _ready() -> void:
+	main_menu_sound.play()
+
 
 func _on_start_game_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/main.tscn")
