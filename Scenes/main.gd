298a38134d74f625a033bf73dd758157456ccab8
Griffin Baxter <GriffinRBaxter@gmail.com>
2024-08-18 12:49:28 +1200

    Clean up main code and node structure

--- Scenes/main.gd
+++ Scenes/main.gd
@@ -6,47 +6,44 @@ var locked_door_and_key_connected = false
 
 var level = 1
 var completed_levels = []
-var level_1_items = []
-var level_2_items = []
+var l1_items_to_hide = []
+var l2_items_to_hide = []
 
-@onready var player: CharacterBody2D = $Node2D/Player
+@onready var player: CharacterBody2D = $Player
 
-# level 1 items (to hide)
-@onready var tile_map_layer_1: TileMapLayer = $Node2D/TileMapLayer1
-@onready var plant: Area2D = $Node2D/Plant
-@onready var label: Label = $UI/MarginContainer/Label
-# level 1 items
-@onready var locked_door_connect: Area2D = $Node2D/LockedDoorConnect
-@onready var key: Area2D = $Node2D/Key
+# level 1
+@onready var l1_tile_map: TileMapLayer = $Level1/TileMapLayer1
+@onready var l1_plant: Area2D = $Level1/Plant
+@onready var l1_label: Label = $UI/MarginContainer/Label
+@onready var l1_locked_door_connect: Area2D = $Level1/LockedDoorConnect
+@onready var l1_key: Area2D = $Level1/Key
 
-# level 2 items
-@onready var tile_map_layer_2: TileMapLayer = $Node2D/TileMapLayer2
+# level 2
+@onready var l2_tile_map: TileMapLayer = $Level2/TileMapLayer2
 
 
 func _ready() -> void:
-	update_level(1, level_1_items, [level_2_items])
-
-	level_1_items = [tile_map_layer_1, plant, label]
-	level_2_items = [tile_map_layer_2]
+	l1_items_to_hide = [l1_tile_map, l1_plant, l1_label]
+	l2_items_to_hide = [l2_tile_map]
 
 
 func _process(_delta: float) -> void:
 	if not completed_levels.has(1):
-		level_1_complete_criteria()
+		l1_complete_criteria()
 
 	if player.global_position.x >= LEVEL_2_X_POS:
-		update_level(1, level_1_items, [level_2_items])
+		update_level(1, l1_items_to_hide, [l2_items_to_hide])
 	else:
-		update_level(2, level_2_items, [level_1_items])
+		update_level(2, l2_items_to_hide, [l1_items_to_hide])
 
 
-func level_1_complete_criteria():
+func l1_complete_criteria():
 	if locked_door_and_key_connected:
-		set_sprite_outline_colour(key, Color.BLACK)
-		if 6 <= key.scale.x and key.scale.x <= 8:
-			unlock_door()
+		set_sprite_outline_colour(l1_key, Color.BLACK)
+		if 6 <= l1_key.scale.x and l1_key.scale.x <= 8:
+			l1_unlock_door()
 	else:
-		set_sprite_outline_colour(key, Color.WHITE)
+		set_sprite_outline_colour(l1_key, Color.WHITE)
 
 
 func set_sprite_outline_colour(sprite, colour):
@@ -62,17 +59,17 @@ func update_level(new_level, visible_items, non_visible_items_array):
 			item.visible = false
 
 
-func unlock_door():
-	var locked_door_tile_pos = tile_map_layer_1.to_local(locked_door_connect.global_position)
-	var locked_door_tile_map_pos = tile_map_layer_1.local_to_map(locked_door_tile_pos)
+func l1_unlock_door():
+	var locked_door_tile_pos = l1_tile_map.to_local(l1_locked_door_connect.global_position)
+	var locked_door_tile_map_pos = l1_tile_map.local_to_map(locked_door_tile_pos)
 
-	var floor_tile_pos = tile_map_layer_1.to_local(key.global_position)
-	var floor_tile_map_pos = tile_map_layer_1.local_to_map(floor_tile_pos)
+	var floor_tile_pos = l1_tile_map.to_local(l1_key.global_position)
+	var floor_tile_map_pos = l1_tile_map.local_to_map(floor_tile_pos)
 
-	var floor_tile = tile_map_layer_1.get_cell_source_id(floor_tile_map_pos)
-	tile_map_layer_1.set_cell(locked_door_tile_map_pos, floor_tile, Vector2i(0, 0))
+	var floor_tile = l1_tile_map.get_cell_source_id(floor_tile_map_pos)
+	l1_tile_map.set_cell(locked_door_tile_map_pos, floor_tile, Vector2i(0, 0))
 
-	key.visible = false
+	l1_key.visible = false
 	completed_levels.append(1)
 
 
