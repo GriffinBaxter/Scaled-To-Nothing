3c7b06709c949fe5e40fd299c7c431c2ce30d47f
Griffin Baxter <GriffinRBaxter@gmail.com>
2024-08-18 20:07:15 +1200

    Add main menu with start/fullscreen options

---
+++ Scripts/main_menu.gd
@@ -0,0 +1,12 @@
+extends Control
+
+
+func _on_start_game_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/main.tscn")
+
+
+func _on_fullscreen_pressed() -> void:
+	if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_WINDOWED:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
