821934ef455169de8c61a061275c489d9fa6b0c7
Griffin Baxter <GriffinRBaxter@gmail.com>
2024-08-18 15:29:23 +1200

    Make level 2 completable

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -2,7 +2,8 @@ extends Node
 
 const LEVEL_2_X_POS = -140
 
-var locked_door_and_key_connected = false
+var l1_door_and_key_connected = false
+var l2_door_and_key_connected = false
 
 var level = 1
 var completed_levels = []
@@ -21,6 +22,9 @@ var global_object_currently_scaling = null
 
 # level 2
 @onready var level_2: Node2D = $Level2
+@onready var l2_tile_map: TileMapLayer = $Level2/TileMapLayer2
+@onready var l2_key: Area2D = $Level2/KeyFlipped
+@onready var l2_locked_door_connect: Area2D = $Level2/LockedDoorConnect
 
 
 func _ready() -> void:
@@ -32,20 +36,13 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
 	if not completed_levels.has(1):
 		l1_complete_criteria()
+	if not completed_levels.has(2):
+		l2_complete_criteria()
 
 	if player.global_position.x >= LEVEL_2_X_POS:
 		update_level(1, l1_items_to_hide, [l2_items_to_hide])
 	else:
 		update_level(2, l2_items_to_hide, [l1_items_to_hide])
-
-
-func l1_complete_criteria():
-	if locked_door_and_key_connected:
-		set_sprite_outline_colour(l1_key, Color.BLACK)
-		if 6 <= l1_key.scale.x and l1_key.scale.x <= 8:
-			l1_unlock_door()
-	else:
-		set_sprite_outline_colour(l1_key, Color.WHITE)
 
 
 func set_sprite_outline_colour(sprite, colour):
@@ -63,6 +60,15 @@ func update_level(new_level, visible_items, non_visible_items_array):
 				item.visible = false
 
 
+func l1_complete_criteria():
+	if l1_door_and_key_connected:
+		set_sprite_outline_colour(l1_key, Color.BLACK)
+		if 6 <= l1_key.scale.x and l1_key.scale.x <= 8:
+			l1_unlock_door()
+	else:
+		set_sprite_outline_colour(l1_key, Color.WHITE)
+
+
 func l1_unlock_door():
 	var locked_door_tile_pos = l1_tile_map.to_local(l1_locked_door_connect.global_position)
 	var locked_door_tile_map_pos = l1_tile_map.local_to_map(locked_door_tile_pos)
@@ -77,16 +83,49 @@ func l1_unlock_door():
 	completed_levels.append(1)
 
 
-func _on_locked_door_connect_area_entered(area: Area2D) -> void:
-	if area.name == "KeyConnect":
-		locked_door_and_key_connected = true
+func l2_complete_criteria():
+	if l2_door_and_key_connected:
+		set_sprite_outline_colour(l2_key, Color.BLACK)
+		if 6 <= l2_key.scale.x and l2_key.scale.x <= 8:
+			l2_unlock_door()
+	else:
+		set_sprite_outline_colour(l2_key, Color.WHITE)
 
 
-func _on_locked_door_connect_area_exited(area: Area2D) -> void:
-	if area.name == "KeyConnect":
-		locked_door_and_key_connected = false
+func l2_unlock_door():
+	var locked_door_tile_pos = l2_tile_map.to_local(l2_locked_door_connect.global_position)
+	var locked_door_tile_map_pos = l2_tile_map.local_to_map(locked_door_tile_pos)
+
+	var floor_tile_pos = l2_tile_map.to_local(l2_key.global_position)
+	var floor_tile_map_pos = l2_tile_map.local_to_map(floor_tile_pos)
+
+	var floor_tile = l2_tile_map.get_cell_source_id(floor_tile_map_pos)
+	l2_tile_map.set_cell(locked_door_tile_map_pos, floor_tile, Vector2i(0, 0))
+
+	l2_key.queue_free()
+	completed_levels.append(2)
 
 
 func _on_object_currently_scaling(value: Variant) -> void:
 	if global_object_currently_scaling == null or value == null:
 		global_object_currently_scaling = value
+
+
+func _l1_on_key_connect(area: Area2D) -> void:
+	if area.name == "KeyConnect":
+		l1_door_and_key_connected = true
+
+
+func _l1_on_key_disconnect(area: Area2D) -> void:
+	if area.name == "KeyConnect":
+		l1_door_and_key_connected = false
+
+
+func _l2_on_key_connect(area: Area2D) -> void:
+	if area.name == "KeyConnect":
+		l2_door_and_key_connected = true
+
+
+func _l2_on_key_disconnect(area: Area2D) -> void:
+	if area.name == "KeyConnect":
+		l2_door_and_key_connected = false
